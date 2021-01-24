d4ec0c014b55fb187b3f5d0ca9e2e09a6b4fc570
Fatih <fulu3517@gmail.com>
2021-01-24 21:04:06 +0300

    Yorumlar alındı

--- src/components/YaziDetay.js
+++ src/components/YaziDetay.js
@@ -3,17 +3,41 @@ import axios from 'axios';
 const YaziDetay = (props) => {
     const {id} = props.match.params;
     const [yaziDetayi, setYaziDetayi] = useState({});
+    const [yorumlar, setYorumlar] = useState([])
     useEffect(() => {
-        axios.get(`https://react-yazi-yorum.herokuapp.com/posts/${id}`)
-        .then(response => setYaziDetayi(response.data))
+        axios
+            .get(`https://react-yazi-yorum.herokuapp.com/posts/${id}`)
+            .then(response => {
+                setYaziDetayi(response.data)
+            });
+        axios
+            .get(`https://react-yazi-yorum.herokuapp.com/posts/${id}/comments`)
+            .then(response => {
+                console.log(response.data);
+                setYorumlar(response.data)
+            })
         // fetch(`https://react-yazi-yorum.herokuapp.com/posts/${id}`)
         // .then(response => response.json())
         // .then(data => setYaziDetayi(data))
-    })
+    },[])
     return <>
-    <h2 className="ui header">{yaziDetayi.title}</h2>
-    <p>{yaziDetayi.created_at}</p>
-    <p>{yaziDetayi.content}</p>
-    </>   
+        <h2 className="ui header">{yaziDetayi.title}</h2>
+        <p>{yaziDetayi.created_at}</p>
+        <p>{yaziDetayi.content}</p>
+        {yorumlar.map((yorum) => {
+            
+            return (
+                <div>
+                    <div>
+                        <p>{yorum.display_name}</p>
+                        <p>{yorum.body}</p>
+                        <p>{yorum.created_at}</p>
+                    </div>
+                    <hr/>
+                </div>
+                
+            )
+        })}
+    </>
 }
 export default YaziDetay;
